2cd760fe4ec90ed3dabe7f6fc0844bf1dae6e691
Emmanuel Kasper <e.kasper@proxmox.com>
2017-12-14 14:25:27 +0100

    Use fixed name for VM, so we can always find the ovf file

--- test.pl
+++ test.pl
@@ -13,13 +13,13 @@ my $disk = $ARGV[0];
 
 die "usage: $0 [ --keep ] disk image" if !$disk;
 
-my ($shortname, $extension) = split/\./, $disk;
+my $vm_name = 'test_vm';
 
-system("./import2vbox.pl --vcpus 2 --memory 384 $disk");
+system("./import2vbox.pl --vcpus 2 --memory 384 $disk --name $vm_name");
 
-#system("VBoxManage import --dry-run${shortname}.ovf");
-system("VBoxManage import ${shortname}.ovf");
-system("ovftool --verifyOnly ${shortname}.ovf");
-system("VBoxManage showvminfo $shortname --machinereadable | grep storagecontrollerportcount0");
-system("VBoxManage unregistervm $shortname --delete") if !$keep_vbox_vm;
+#system("VBoxManage import --dry-run${vm_name}.ovf");
+system("VBoxManage import ${vm_name}.ovf");
+system("ovftool --verifyOnly ${vm_name}.ovf");
+system("VBoxManage showvminfo $vm_name --machinereadable | grep storagecontrollerportcount0");
+system("VBoxManage unregistervm $vm_name --delete") if !$keep_vbox_vm;
 
